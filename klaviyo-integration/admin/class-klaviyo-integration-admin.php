29218fd41258c852d1483288806c96eb397de960
sagarbhattarai609 <sagar@pcsoftnepal.com>
2023-09-20 17:09:11 +0545

    testing to fetch contact from fields to map

--- klaviyo-integration/admin/class-klaviyo-integration-admin.php
+++ klaviyo-integration/admin/class-klaviyo-integration-admin.php
@@ -209,5 +209,140 @@ class Klaviyo_Integration_Admin {
 			// exit;
 		}
 
+
+
+
+
+
+
+		public function KLCF_admin_after_additional_settings() {
+
+			$post_id = sanitize_text_field($_GET['post']);
+			$enable =	get_post_meta($post_id, "_KLCF_enable", true); 
+			$f_data = get_post_meta($post_id, "_KLCF_data", true); 
+			$enable_tag = get_post_meta($post_id, "_KLCF_enable_tag", true); 
+			$custom_tag = get_post_meta($post_id, "_KLCF_custom_tag", true);
+
+			$manager = WPCF7_FormTagsManager::get_instance();
+	
+			$form_tags = $manager->get_scanned_tags();
+	
+			$form_scan_tags = $this->tags_formate_array($form_tags);
+			$auto_field_selected = $this->KLCF_searchField('email',$form_tags);
+	
+			$map_fields = array(
+	
+					[
+						'name' => 'email',
+						'key' => '$email',
+						'required' => true
+					]
+				);
+
+				var_dump($map_fields);
+				exit;
+	
+			if(empty($f_data)){
+			
+				$map_fields[] =	    
+					[
+						'name' => 'first name',
+						'key'  => '$first_name',
+						'required' => false
+					];
+				$map_fields[] =
+					[
+						'name' => 'last name',
+						'key'  => '$last_name',
+						'required' => false
+					];			
+			}else{
+	
+				$map_field[] =	    
+					[
+						'name' => 'first name',
+						'key'  => '$first_name',
+						'required' => false
+					];
+				$map_field[] =
+					[
+						'name' => 'last name',
+						'key'  => '$last_name',
+						'required' => false
+					];
+	
+				$keys = array_keys($f_data);					
+				foreach($map_field as $row){
+					if(in_array($row['key'],$keys)){
+						$map_fields[] = $row;
+					}
+				}
+			}
+	
+			// if()
+	
+			$map_extra_fields = array(
+				[
+					'name' => 'First Name',
+					'key'  => '$first_name',
+					'required' => false
+				],
+				[
+					'name' => 'Last Name',
+					'key'  => '$last_name',
+					'required' => false
+				],
+				[
+					'name' => 'Phone',
+					'key'  => '$phone_number',
+					'required' => false
+				],
+				[
+					'name' => 'City',
+					'key'  => '$city',
+					'required' => false
+				],
+				[
+					'name' => 'Region',
+					'key'  => '$region',
+					'required' => false
+				],
+				[
+					'name' => 'Country',
+					'key'  => '$country',
+					'required' => false
+				],
+				[
+					'name' => 'Zip',
+					'key'  => '$zip',
+					'required' => false
+				],
+	
+			);
+	
+			// include('templates/intregation-form.php');
+			return 0;
+		}
+	
+
+
+
+   
+        // $this->KLCF_admin_after_additional_settings();
 	
 }
+
+// $z = new Klaviyo_Integration_Admin;
+// $z->KLCF_admin_after_additional_settings();
+
+
+
+// foreach($form as $objForm){
+// 	$manager = WPCF7_FormTagsManager::get_instance();
+// 	$tags  = $manager->scan( $objForm->form );
+// 	$filter_result = $manager->filter( $tags, $cond );
+// 	 foreach ($filter_result as $key => $value) {
+// 		 echo $value->type;
+// 		 echo $value->name;
+// 	 }
+//   }
--- klaviyo-integration/admin/js/klaviyo-integration-admin.js
+++ klaviyo-integration/admin/js/klaviyo-integration-admin.js
@@ -29,23 +29,55 @@
    * practising this, we should strive to set a better example in our own work.
    */
   var all_klaviyo_list = [];
-  var apiKey;
+  var apiKey ;
   let local_storage = localStorage.getItem("ak7_apiKey");
+  var selected_klaviyo_list = "RBEBce";
+  //   var apiKey="pk_2e7ce7bd3820c5309c254207cc681e3016";
+  //   var selected_klaviyo_list = "SyNiES" //test
+  //   var apiKey="pk_5b2f7c44386b67d73ed184f059ac862eb0"; // test
 
-  if(!local_storage == ""){
+    //if(!local_storage == ""){
 	// $(".a_cf7_ki fieldset").append(append_data);
 	// $(".a_cf7_ki ._h2 span").css("color", "#1ed41e").text("Enabled");
 	// $("#apiKey").val(local_storage);
-	// append_fetched_data()
- }
+	// append_ui()
+    // }
 
   $(document).on("click", "#a-cf7-custom-field", function () {
-    var input_value = $("#a-cf7-custom-field").val();
-    if (input_value == "") {
-      input_value = $("#a-cf7-custom-field").val("checked");
-    } else {
-      input_value = $("#a-cf7-custom-field").val();
-    }
+    enter_your_api_key_block();
+  });
+  $(document).on("click", ".api_input_box ._button", function (e) {
+    apiKey = $("#apiKey").val();
+    if(local_storage == null){
+		localStorage.setItem("ak7_apiKey", apiKey);
+	}
+    // var test_call = 'https://a.klaviyo.com/api/v2/lists?api_key='+apiKey
+    // console.log("api call", test_call);
+	if(apiKey == ""){
+		e.preventDefault();
+		alert("Please Enter Your Valid Api Key");
+		return 0;
+	 }else{
+		get_all_forms_list_from_klaviyo(e);
+	 }
+  });
+
+  $(document).on("change", ".select_lists .f_list option", function () {
+	console.log("$(this)",$(this));
+    selected_klaviyo_list = $(this).val();
+	//console.log("selected_klaviyo_list",selected_klaviyo_list);
+	get_list_fields()
+  });
+function enter_your_api_key_block(){
+	var input_value = $("#a-cf7-custom-field").val();
+    // if (input_value == "") {
+    //   input_value = $("#a-cf7-custom-field").val("checked");
+    // } else {
+    //   input_value = $("#a-cf7-custom-field").val();
+    // }
+	if (input_value == "") {
+		input_value = $("#a-cf7-custom-field").val("checked");
+	}
 
     if (input_value[0].defaultValue == "checked" || input_value == "checked") {
       var append_data = `
@@ -59,61 +91,66 @@
 									  </div>
 							   </div>
 			`;
-      var found = jQuery(".a_cf7_ki").find(".api_input_box");
+      var found = $(".a_cf7_ki").find(".api_input_box");
       if (found.length == 0) {
         $(".a_cf7_ki fieldset").append(append_data);
-        $(".a_cf7_ki ._h2 span").css("color", "#1ed41e").text("Enabled");
+        // $(".a_cf7_ki ._h2 span").css("color", "#1ed41e").text("Enabled");
 		if(!local_storage == ""){
 			$("#apiKey").val(local_storage);
 			apiKey = local_storage;
 			if(all_klaviyo_list.length > 0){
-				append_fetched_data();
+				append_ui(); 
 			}else{
-				get_list_from_klaviyo();
+				get_all_forms_list_from_klaviyo();
 			}			
 		 }	
       } else {
         input_value = $("#a-cf7-custom-field").val();
         $(".a_cf7_ki .api_input_box, #has_lists ").remove();
-        $(".a_cf7_ki ._h2 span").css("color", "#d10707").text("Disabled");
+        // $(".a_cf7_ki ._h2 span").css("color", "#d10707").text("Disabled");
       }
     } else {
       $(".a_cf7_ki .api_input_box, #has_lists").remove();
     }
-  });
-
-  $(document).on("click", ".api_input_box ._button", function (e) {
-    apiKey = $("#apiKey").val();
-    if(local_storage == null){
-		localStorage.setItem("ak7_apiKey", apiKey);
-	}
-    // var test_call = 'https://a.klaviyo.com/api/v2/lists?api_key='+apiKey
-    // console.log("api call", test_call);
-    get_list_from_klaviyo(e);
-  });
-
-  function get_list_from_klaviyo(e) {
+}
+  function get_all_forms_list_from_klaviyo(e) {
 	if(!e == ""){
 		e.preventDefault();
 	}
 	loader();
-    const options = { method: "GET", headers: { accept: "application/json" } };
+    const options = { method: "GET", headers: { accept: "application/json", Authorization: apiKey } };
     fetch("https://a.klaviyo.com/api/v2/lists?api_key=" + apiKey, options)
       .then((response) => response.json())
       .then((response) => {
         all_klaviyo_list = response;
+		//console.log("all_klaviyo_list", all_klaviyo_list);
 		if (all_klaviyo_list.length > 0) {
-		  	append_fetched_data();
+		  	append_ui();
 			loader();
 		}
       })
       .catch((err) => console.error(err));
   }
 
-  function append_fetched_data(){
+  function get_list_fields(){
+	  const options = {
+		 method: "GET", 
+		 headers: { 
+			accept: "application/json", 
+			"revision": "2023-05-11",
+			"content-type": "application/json",
+			Authorization: "Klaviyo-API-Key"+apiKey } };
 
-		// $(".api_input_box ._button").fadeOut();
-		$(".api_input_box ._button").remove();
+	  console.log("options::",options)
+	  fetch('https://a.klaviyo.com/api/lists/'+selected_klaviyo_list, options)
+		.then(response => response.json())
+		.then(response => console.log("response for get_list_fields",response))
+		.catch(err => console.error(err));
+  }
+
+  function append_ui(){
+		// $(".api_input_box ._button").remove();
+		$(".api_input_box ._button").hide();
 		$(".api_input_box").append(`
 									<div class="select_lists">
 									     <label>Select Klaviyo List</label>
@@ -122,83 +159,93 @@
 		`);
 
 		$(".a_cf7_ki").append(`
-								 <div id="has_lists">
-									<fieldset class="map-fields">
-										<h2 class="head_ing">Map Fields:</h2>
-										<div class="form-group add_block">
-											<div class="row">
-											<div class="col-md-8">
-												<div class="col-md-4">
-													<label>Email<span> *</span></label>
-												</div>
-												<div class="col-md-8">
-													<select class="form-control" required="" name="KLCF_data[$email]">
-														<option value="0">Select</option>
-														<option value="your-name">Your Name</option>
-														<option value="your-email">Your Email</option>
-														<option value="your-subject">Your Subject</option>
-														<option value="your-message">Your Message</option>
-													</select>
-												</div>
-											</div>
-											</div>
-										</div>
-										<div class="form-group add_block">
-											<div class="row">
-											<div class="col-md-8">
-												<div class="col-md-4">
-													<label>First Name</label>
-												</div>
-												<div class="col-md-8">
-													<select class="form-control" required="" name="KLCF_data[$first_name]">
-														<option value="0">Select</option>
-														<option value="your-name">Your Name</option>
-														<option value="your-email">Your Email</option>
-														<option value="your-subject">Your Subject</option>
-														<option value="your-message">Your Message</option>
-													</select>
-												</div>
-											</div>
-											<div class="col-md-4"> <a class="btn_ btn-danger_" onclick="KLCF_remove_custom_field(this);"><img src="/wp-content/plugins/klaviyo-integration/admin/images/delete.svg"/></a>
-											</div>
-											</div>
-										</div>
-										<div class="form-group add_block">
-											<div class="row">
-											<div class="col-md-8">
-												<div class="col-md-4">
-													<label>Last Name</label>
-												</div>
-												<div class="col-md-8">
-													<select class="form-control" required="" name="KLCF_data[$last_name]">
-														<option value="0">Select</option>
-														<option value="your-name">Your Name</option>
-														<option value="your-email">Your Email</option>
-														<option value="your-subject">Your Subject</option>
-														<option value="your-message">Your Message</option>
-													</select>
-												</div>
-											</div>
-											<div class="col-md-4"> <a class="btn_ btn-danger_" onclick="KLCF_remove_custom_field(this);"><img src="/wp-content/plugins/klaviyo-integration/admin/images/delete.svg"/></a>
-											</div>
-											</div>
-										</div>
-										<div id="add_on_fields">
-										</div>
-										<div class="row">
-											<div class="col-md-12">
-											<div class="col-md-4 pull-right">
-												<a class="btn_ btn-primary_ btn-full" onclick="KLCF_add_custom_field();"><img src="/wp-content/plugins/klaviyo-integration/admin/images/add.svg"/></a>
-											</div>
-											</div>
-										</div>
-								</fieldset>
-								 </div>
+		<div id="has_lists">
+		<fieldset class="map-fields">
+
+		  <div class="row">
+             <div class="col-md-9"> 
+			 <h2 class="head_ing">Map Fields:</h2>
+			 <div class="form-group add_block">
+				 <div class="row">
+				 <div class="col-md-9">
+					 <div class="col-md-4">
+						 <label>Email<span> *</span></label>
+					 </div>
+					 <div class="col-md-8">
+						 <select class="form-control" required="" name="">
+							 <option value="0">Select</option>
+							 <option value="your-name">Your Name</option>
+							 <option value="your-email">Your Email</option>
+							 <option value="your-subject">Your Subject</option>
+							 <option value="your-message">Your Message</option>
+						 </select>
+					 </div>
+				 </div>
+				 </div>
+			 </div>
+			 <div class="form-group add_block">
+				 <div class="row">
+				 <div class="col-md-9">
+					 <div class="col-md-4">
+						 <label>First Name</label>
+					 </div>
+					 <div class="col-md-8">
+						 <select class="form-control" required="" name="">
+							 <option value="0">Select</option>
+							 <option value="your-name">Your Name</option>
+							 <option value="your-email">Your Email</option>
+							 <option value="your-subject">Your Subject</option>
+							 <option value="your-message">Your Message</option>
+						 </select>
+					 </div>
+				 </div>
+				 <div class="col-md-3 delete"> <a class="btn_ btn-danger_"><img src="/wp-content/plugins/klaviyo-integration/admin/images/delete.svg"/>Remove</a>
+				 </div>
+				 </div>
+			 </div>
+			 <div class="form-group add_block">
+				 <div class="row">
+				 <div class="col-md-9">
+					 <div class="col-md-4">
+						 <label>Last Name</label>
+					 </div>
+					 <div class="col-md-8">
+						 <select class="form-control" required="" name="">
+							 <option value="0">Select</option>
+							 <option value="your-name">Your Name</option>
+							 <option value="your-email">Your Email</option>
+							 <option value="your-subject">Your Subject</option>
+							 <option value="your-message">Your Message</option>
+						 </select>
+					 </div>
+				 </div>
+				 <div class="col-md-3 delete"> <a class="btn_ btn-danger_"><img src="/wp-content/plugins/klaviyo-integration/admin/images/delete.svg"/>Remove</a>
+				 </div>
+				 </div>
+			 </div>
+			 <div id="add_on_fields">
+			 </div>
+			 <div class="row">
+				 <div class="col-md-12">
+				 <div class="col-md-3 pull-right add">
+					 <a class=" btn_ btn-primary_ btn-full" ><img src="/wp-content/plugins/klaviyo-integration/admin/images/add.svg"/>Add Field</a>
+				 </div>
+				 </div>
+			 </div>
+		     </div>
+
+			 <div class="col-md-3"> 
+			 <button id="cak">Change Api Key</button>
+		 </div>
+		   </div>
+		</fieldset>	
 			`);
 
 		$.each(all_klaviyo_list, function(key,list) {              
-			$('.select_lists .f_list').append('<option value="0">'+list.list_name+'</option>');
+			$('.select_lists .f_list').append('<option value="'+list.list_id+'">'+list.list_name+'</option>');
 		});     
+		$('#apiKey').attr('disabled','disabled');
+		$(".a_cf7_ki ._h2 span").css("color", "#1ed41e").text("Enabled");
   }
 
   function loader(){
@@ -210,4 +257,42 @@
 		$('.aki7_loader').show();
 	}
   }
+  $(document).on("click", ".add", function () {
+	custom_adding_field_block()
+  });
+  $(document).on("click", ".delete", function () {
+	$(this).closest(".add_block").remove();
+  });
+  $(document).on("click", "#cak", function (e) {
+	e.preventDefault();
+	localStorage.removeItem("ak7_apiKey");
+	$("#apiKey").val("").removeAttr('disabled');
+	$(".api_input_box ._button").show();
+	$('#has_lists,.select_lists').remove();
+	$(".a_cf7_ki ._h2 span").css("color", "#d10707").text("Disabled");
+  });
+  function custom_adding_field_block(){
+    $("#add_on_fields").append(`
+				<div class="form-group add_block">
+				<div class="row">
+				<div class="col-md-9">
+					<div class="col-md-4">
+					<select class="form-control" required="" name="">
+					   <option value="0">Select</option>
+				    </select>
+					</div>
+					<div class="col-md-8">
+						<select class="form-control" required="" name="">
+							<option value="0">Select</option>
+						</select>
+					</div>
+				</div>
+				<div class="col-md-3 delete"> <a class="btn_ btn-danger_"><img src="/wp-content/plugins/klaviyo-integration/admin/images/delete.svg"/>Remove</a>
+				</div>
+				</div>
+			</div>
+	`)
+  }
+
+
 })(jQuery);
